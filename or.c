4bbed8a4f58a26b5ee31387fba0bfe43795a8c64
30919 <129143276+ssoyeonnn@users.noreply.github.com>
2023-04-11 05:15:52 +0000

    홀수 짝수

--- or.c
+++ or.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
- int main(void) 
- {
-   int a=15;
-   int b=20;
-   int c = a|b;
+// #include <stdio.h>
+//  int main(void) 
+//  {
+//    int a=15;
+//    int b=20;
+//    int c = a|b;
   
-   printf("OR 연산 결과 : %d",c);
- }
+//    printf("OR 연산 결과 : %d",c);
+//  }
