a57654ed4ca29a290e849dc34a85fa979f51cf03
ssoyeonnn <129143276+ssoyeonnn@users.noreply.github.com>
2023-05-15 02:17:25 +0000

    조건문 끝!

--- mission_myscore.c
+++ mission_myscore.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-main(){
-    int a;
-  printf("1:90점이상, 2:80점이상, 3:70점이상, 4:60점이상, 5:60점미만");
-  scanf ("%d",&a);
+// #include<stdio.h>
+// main(){
+//     int a;
+//   printf("1:90점이상, 2:80점이상, 3:70점이상, 4:60점이상, 5:60점미만");
+//   scanf ("%d",&a);
 
-  if(a==1)
-    printf("A학점\n");
-  else if(a==2)
-    printf("B학점\n");
-  else if(a==3)
-    printf("c학점\n");
-  else if(a==4)
-    printf("D학점\n");
-  else
-    printf("F학점\n");
- printf("학점 조사 끝");
-}
+//   if(a==1)
+//     printf("A학점\n");
+//   else if(a==2)
+//     printf("B학점\n");
+//   else if(a==3)
+//     printf("c학점\n");
+//   else if(a==4)
+//     printf("D학점\n");
+//   else
+//     printf("F학점\n");
+//  printf("학점 조사 끝");
+// }
